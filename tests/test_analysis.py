aec74705cb6dd9a823b53e01b30d18cbaf1ddbbe
speedcell4 <speedcell4@gmail.com>
2019-03-25 14:47:08 +0900

    test(analysis): Update generator

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -5,34 +5,62 @@ from hypothesis import given, strategies as st
 from aku import Aku
 
 
+@st.composite
+def integers(draw):
+    a = draw(st.integers())
+    return f'{a}', a
+
+
+@st.composite
+def floats(draw):
+    a = draw(st.floats(min_value=0))
+    return f'{a}', a
+
+
+@st.composite
+def booleans(draw):
+    a = draw(st.booleans())
+    if a:
+        r = draw(st.sampled_from(['1', 't', 'true', 'y', 'yes']))
+    else:
+        r = draw(st.sampled_from(['0', 'f', 'false', 'n', 'no']))
+    return r, a
+
+
+@st.composite
+def strings(draw):
+    a = draw(st.text(min_size=1, alphabet=ascii_letters))
+    return a, a
+
+
+@st.composite
+def null(draw):
+    r = draw(st.sampled_from(['nil', 'none', 'null']))
+    return r, None
+
+
 def foo(a: int = 1, b: float = 2, c: complex = 3 + 4j, d: bool = True, e: str = 'e :: string'):
     return locals()
 
 
 @given(
-    a=st.integers(),
-    b=st.floats(min_value=0),  # TODO check option
+    a=integers(),
+    b=floats(),  # TODO check option
     # c=st.complex_numbers(min_magnitude=0),
-    d=st.sampled_from([  # TODO generate case
-        '0', 'f', 'false', 'n', 'no',
-        '1', 't', 'true', 'y', 'yes',
-    ]),
-    e=st.text(min_size=1, alphabet=ascii_letters),
+    d=booleans(),
+    e=strings(),
 )
 def test_foo(a, b, d, e):
     app = Aku()
     app.register(foo)
     ret = app.run([
-        '--a', f'{a}',
-        '--b', f'{b}',
-        # '--c', f'{c}',
-        '--d', f'{d}',
-        '--e', f'{e}',
+        '--a', f'{a[0]}',
+        '--b', f'{b[0]}',
+        # '--c', f'{c[0]}',
+        '--d', f'{d[0]}',
+        '--e', f'{e[0]}',
     ])
-    assert ret['a'] == a
-    assert ret['b'] == b
-    if d in ('0', 'f', 'false', 'n', 'no'):
-        assert not ret['d']
-    else:
-        assert ret['d']
-    assert ret['e'] == e
+    assert ret['a'] == a[1]
+    assert ret['b'] == b[1]
+    assert ret['d'] == d[1]
+    assert ret['e'] == e[1]
