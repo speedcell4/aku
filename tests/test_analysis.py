00d3c9d984bbf9e05d37036e3537a85633d7be55
speedcell4 <speedcell4@gmail.com>
2019-03-25 15:28:32 +0900

    refactor(test): Move strategies to a single file

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,46 +1,10 @@
 import inspect
-from string import ascii_letters
 from typing import List, Tuple, Union
 
 from hypothesis import given, strategies as st
 
 from aku import Aku
-
-
-@st.composite
-def integers(draw):
-    a = draw(st.integers())
-    return f'{a}', a
-
-
-@st.composite
-def floats(draw):
-    a = draw(st.floats(min_value=0))
-    return f'{a}', a
-
-
-@st.composite
-def booleans(draw):
-    a = draw(st.booleans())
-    if a:
-        r = draw(st.sampled_from(['1', 't', 'true', 'y', 'yes']))
-    else:
-        r = draw(st.sampled_from(['0', 'f', 'false', 'n', 'no']))
-    return r, a
-
-
-@st.composite
-def strings(draw):
-    a = draw(st.text(min_size=1, alphabet=ascii_letters))
-    return a, a
-
-
-@st.composite
-def optional(draw, strategy):
-    if draw(st.booleans()):
-        return draw(strategy)
-    r = draw(st.sampled_from(['nil', 'none', 'null']))
-    return r, None
+from tests.strategies import booleans, floats, integers, optional, strings
 
 
 def foo(a: int = 1, b: float = 2, c: complex = 3 + 4j, d: bool = True, e: str = 'e :: string'):
