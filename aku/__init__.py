172ad0eb913b7bcbc88954f575f4490a7d2f3544
speedcell4 <speedcell4@gmail.com>
2020-04-25 13:47:48 +0900

    Feat: Support get_origin and get_args for Python 3.7

--- aku/__init__.py
+++ aku/__init__.py
@@ -2,9 +2,11 @@ import sys
 from typing import List, Tuple, Set, FrozenSet, Dict
 from typing import Optional
 from typing import Union, Type
+from typing import get_type_hints
 
 if sys.version_info < (3, 8):
-    from typing import _SpecialForm
+    import collections
+    from typing import _SpecialForm, _GenericAlias, Generic
 
     Literal = _SpecialForm('Literal', doc=
     """Special typing form to define literal types (a.k.a. value types).
@@ -27,6 +29,46 @@ if sys.version_info < (3, 8):
    is allowed as type argument to Literal[...], but type checkers may
    impose restrictions.
     """)
+
+
+    def get_origin(tp):
+        """Get the unsubscripted version of a type.
+
+        This supports generic types, Callable, Tuple, Union, Literal, Final and ClassVar.
+        Return None for unsupported types. Examples::
+
+            get_origin(Literal[42]) is Literal
+            get_origin(int) is None
+            get_origin(ClassVar[int]) is ClassVar
+            get_origin(Generic) is Generic
+            get_origin(Generic[T]) is Generic
+            get_origin(Union[T, int]) is Union
+            get_origin(List[Tuple[T, T]][int]) == list
+        """
+        if isinstance(tp, _GenericAlias):
+            return tp.__origin__
+        if tp is Generic:
+            return Generic
+        return None
+
+
+    def get_args(tp):
+        """Get type arguments with all substitutions performed.
+
+        For unions, basic simplifications used by Union constructor are performed.
+        Examples::
+            get_args(Dict[str, int]) == (str, int)
+            get_args(int) == ()
+            get_args(Union[int, Union[T, int], str][int]) == (int, str)
+            get_args(Union[int, Tuple[T, int]][str]) == (int, Tuple[str, int])
+            get_args(Callable[[], T][int]) == ([], int)
+        """
+        if isinstance(tp, _GenericAlias):
+            res = tp.__args__
+            if get_origin(tp) is collections.abc.Callable and res[0] is not Ellipsis:
+                res = (list(res[:-1]), res[-1])
+            return res
+        return ()
 else:
     from typing import Literal
 
@@ -35,4 +77,5 @@ __all__ = [
     'Optional',
     'Union', 'Type',
     'Literal',
+    'get_args', 'get_origin', 'get_type_hints',
 ]
