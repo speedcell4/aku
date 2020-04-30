6f7387ac2cff7009064c25f4d78623a285f6ac9f
speedcell4 <speedcell4@gmail.com>
2020-04-30 21:16:44 +0900

    Feat: Add TypeTp

--- aku/tp.py
+++ aku/tp.py
@@ -1,11 +1,26 @@
 import re
 from abc import ABCMeta, abstractmethod
 from argparse import ArgumentParser, SUPPRESS
-from typing import get_args, get_origin, Any, Union, Literal
+from inspect import getfullargspec
+from itertools import zip_longest
+from typing import get_args, get_origin, get_type_hints, Any, Union, Literal
 
 from aku.parse_fn import get_parse_fn
 
 COMMA = re.compile(r'\s*,\s*')
+
+
+def get_type_annotations(tp):
+    tys = get_type_hints(tp)
+    spec = getfullargspec(tp)
+
+    name_default = zip_longest(
+        reversed(spec.args),
+        reversed(spec.defaults or []),
+        fillvalue=SUPPRESS,
+    )
+    for arg_name, arg_default in reversed(list(name_default)):
+        yield arg_name, arg_default, tys[arg_name]
 
 
 class Tp(object, metaclass=ABCMeta):
@@ -44,6 +59,16 @@ class Tp(object, metaclass=ABCMeta):
         if origin is frozenset:
             return FrozenSetTp(origin, cls[args[0]])
 
+        if origin is type:
+            if get_origin(args[0]) is Union:
+                args = get_args(args[0])
+                return UnionTp(str, **{a.__name__: a for a in args})
+            return TypeTp(args[0])
+
+        if origin is Union:
+            args = [get_args(a)[0] for a in args]
+            return UnionTp(str, **{a.__name__: a for a in args})
+
         raise NotImplementedError(f'unsupported {cls.__name__} {tp}')
 
     @property
@@ -61,6 +86,22 @@ class Tp(object, metaclass=ABCMeta):
             type=self.parse_fn, metavar=self.metavar, required=default == SUPPRESS,
             default=repr(default) if default != SUPPRESS else SUPPRESS,
         )
+
+
+class TypeTp(Tp):
+    @property
+    def metavar(self) -> str:
+        raise NotImplementedError
+
+    def parse_fn(self, option_string: str) -> Any:
+        raise NotImplementedError
+
+    def add_argument(self, argument_parser: ArgumentParser, name: str, default: Any):
+        for arg_name, arg_default, arg_tp in get_type_annotations(self.origin):
+            Tp[arg_tp].add_argument(
+                argument_parser=argument_parser,
+                name=arg_name, default=arg_default,
+            )
 
 
 class PrimitiveTp(Tp):
@@ -154,3 +195,12 @@ class FrozenSetTp(Tp):
 
         option_strings = re.split(COMMA, option_string[1:-1])
         return self.origin(self.args[0].parse_fn(s) for s in option_strings)
+
+
+class UnionTp(Tp):
+    @property
+    def metavar(self) -> str:
+        return f"{{{', '.join(self.kwargs.keys())}}}"
+
+    def parse_fn(self, option_string: str) -> Any:
+        return str(option_string.strip())
