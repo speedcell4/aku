fdac8cb06ba0d4fcaa0e76c8993fb2ffa80dbddc
speedcell4 <speedcell4@gmail.com>
2022-05-11 21:26:32 +0900

    Fix: Resolve AkuFn default formatter

--- aku/aku.py
+++ aku/aku.py
@@ -9,6 +9,21 @@ from aku.utils import _init_argument_parser, fetch_name, AKU_FN, AKU
 
 
 class AkuFormatter(ArgumentDefaultsHelpFormatter):
+    def _expand_help(self, action):
+        params = dict(vars(action), prog=self._prog)
+        if params['dest'].endswith(AKU_FN) and isinstance(params['default'], tuple):
+            params['default'] = params['default'][1]
+        for name in list(params):
+            if params[name] is SUPPRESS:
+                del params[name]
+        for name in list(params):
+            if hasattr(params[name], '__name__'):
+                params[name] = params[name].__name__
+        if params.get('choices') is not None:
+            choices_str = ', '.join([str(c) for c in params['choices']])
+            params['choices'] = choices_str
+        return self._get_help_string(action) % params
+
     def _format_actions_usage(self, actions, groups):
         required_option_strings = [
             action.option_strings[-1][2:]
